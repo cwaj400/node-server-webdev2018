35ce384096f703d5b99292b24a787bfda04e97f6
Will Angell-James <william.angelljames9@gmail.com>
2018-08-10 17:52:10 -0400

    headers

--- server.js
+++ server.js
@@ -4,14 +4,14 @@ var express = require('express');
 var app = express();
 var session = require('express-session');
 var bodyParser = require('body-parser');
-// const MONGO_DB_URL = 'mongodb://cwaj400:rootuser1@ds223609.mlab.com:23609/webdevsummer2assfour';
+const MONGO_DB_URLS = 'mongodb://cwaj400:rootuser1@ds223609.mlab.com:23609/webdevsummer2assfour';
 
 const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGO_DB_URL);
 
 
 app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
+    res.header('Access-Control-Allow-Origin', 'https://angular-app-angelljames.herokuapp.com');
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
